cf0fb6b59b46abcc4f9271893483bd73e21a7d35
Sahnawaz Hussain <sahnawaz643786@gmail.com>
2025-08-06 22:07:11 +0530

    Added user validation

--- src/routes/v1/UserRoutes.js
+++ src/routes/v1/UserRoutes.js
@@ -3,11 +3,12 @@ import { Router } from "express";
 import { StatusCodes } from "http-status-codes";
 import { User } from "../../models/userSchema.js";
 import bcrypt from 'bcrypt';
+import { userDataValidation } from "../../utils/userValidation.js";
 
 const UserRoutes=Router();
 
 //Routes for Creating User
-UserRoutes.post("/user",async (req,res)=>{
+UserRoutes.post("/user",userDataValidation,async (req,res)=>{
     const {name,email,password}=req.body;
     
     const hashPassword=await bcrypt.hash(password, 10);
@@ -20,11 +21,11 @@ UserRoutes.post("/user",async (req,res)=>{
     })
     res.status(StatusCodes.OK).json({
         msg:"New User created",
-        response:User
+        response:user
     })
     }catch(err){
         res.status(StatusCodes.BAD_REQUEST).json({
-            error:err
+            error:err.message
         })
     }
 })
---
+++ src/utils/userValidation.js
@@ -0,0 +1,23 @@
+import validator from "validator";
+
+const userDataValidation = (req, res, next) => {
+    const { name, email, password } = req.body;
+
+    console.log("Ye bhi dekh le Bhai");
+
+    if (!name || name.trim().length < 2) {
+        return res.status(400).json({ error: "Name must be at least 2 characters." });
+    }
+
+    if (!email || !validator.isEmail(email)) {
+        return res.status(400).json({ error: "Invalid email." });
+    }
+
+    if (!password || password.length < 6||!validator.isStrongPassword(password)) {
+        return res.status(400).json({ error: "Plese make Password Strong" });
+    }
+
+    next(); 
+};
+
+export { userDataValidation };
