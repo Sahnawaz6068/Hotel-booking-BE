72999452dad85dec5214e594dce9e56762c35ba4
Sahnawaz Hussain <sahnawaz643786@gmail.com>
2025-08-07 13:44:28 +0530

    Added AI endpoint ,Time to fine tune this ai for our application

---
+++ src/ai.js
@@ -0,0 +1,28 @@
+import { GoogleGenAI } from "@google/genai";
+import { config } from "dotenv";
+config({path:"./.env"})
+const key=process.env.GEMINI_API_KEY;
+console.log(key);
+const ai = new GoogleGenAI({ apiKey: key});
+
+export async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: "What is presedient of USA",
+    config: {
+      systemInstruction: `You are a Data structure and Algorithm Instructor. You will only reply to the problem related to 
+      Data structure and Algorithm. You have to solve query of user in simplest way
+      If user ask any question which is not related to Data structure and Algorithm, reply him rudely
+      Example: If user ask, How are you
+      You will reply: You dumb ask me some sensible question, like this message you can reply anything more rudely
+      
+      You have to reply him rudely if question is not related to Data structure and Algorithm.
+      Else reply him politely with simple explanation`,
+    },
+  });
+  console.log(response.text);
+  return response.text
+}
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,38 +1,63 @@
 import express from "express";
 import { config } from "dotenv";
 import { connectionDB } from "./utils/db.js";
+import cors from "cors"; 
 
-import {HotelRoutes} from './routes/v1/HotelRoutes.js';
+import { HotelRoutes } from './routes/v1/HotelRoutes.js';
 import { UserRoutes } from "./routes/v1/UserRoutes.js";
 import { bookRoom } from "./routes/v1/bookRoom.js";
+import { main } from "./ai.js";
+import { StatusCodes } from "http-status-codes";
 
 const app = express();
+
+
+app.use(cors({
+  origin: "http://localhost:5173/", 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
 app.use(express.json());
 
 config({ path: "./.env" });
+
 const PORT = process.env.PORT;
-const DB_URL=process.env.DB_URL;
+const DB_URL = process.env.DB_URL;
 
 console.log("Reach1");
-//Route Setup
-app.use("/api/v1",HotelRoutes);
-app.use("/api/v1",UserRoutes);
-app.use("/api/v1",bookRoom);
 
+// Routes
+app.use("/api/v1", HotelRoutes);
+app.use("/api/v1", UserRoutes);
+app.use("/api/v1", bookRoom);
 
-async function startApp(){
-    try{
-        await connectionDB(DB_URL);
-        console.log("db is connected")
-        app.listen(PORT,()=>{
-            console.log(`App is listening on port: ${PORT}`)
-        });
-    }catch(err){
-        console.error(err.message)
+//ai routes
+app.post("/api/ai", async (req,res)=>{
+  const text=req.body.prompt;
+  try{
+    const response=await main(text);
+      res.status(StatusCodes.OK).json({
+        response:response
+    })
+  }catch(err){
+    res.status(StatusCodes.BAD_REQUEST).json({
+      error:err.message
+    })
+  }
+})
 
-        console.log("something wrong happen")
-    }
-    
-} 
+async function startApp() {
+  try {
+    await connectionDB(DB_URL);
+    console.log("db is connected");
+    app.listen(PORT, () => {
+      console.log(`App is listening on port: ${PORT}`);
+    });
+  } catch (err) {
+    console.error(err.message);
+    console.log("something wrong happen");
+  }
+}
 
 startApp();
